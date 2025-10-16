76bafe9c49e4f98a9ce69b586fa2f49ec611a86d
Tanishq raina <1ms23cs194@msrit.edu>
2025-10-16 14:47:14 +0530

    Updated project structure and fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a simple file that takes some numbers as input and adds them
+Now i am doing some modification for commiting
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 x = int(input("Enter first number:"))
 y = int(input("Enter second number:"))
+z = int(input("Enter third number:"))
 
-print("Addition of numbers:",(x+y))
+print("Addition of numbers:",(x+y+z))
