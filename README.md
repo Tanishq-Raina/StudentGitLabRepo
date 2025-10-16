8b8721608ca55d587442251cd7d086a47edab2a2
Tanishq raina <1ms23cs194@msrit.edu>
2025-10-16 14:28:49 +0530

    Initial commit with README and main.py

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple file that takes some numbers as input and adds them
---
+++ main.py
@@ -0,0 +1,4 @@
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+
+print("Addition of numbers:",(x+y))
