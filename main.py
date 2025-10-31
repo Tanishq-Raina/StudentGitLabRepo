917e78ea279fc73728601b1b5494225e85760ad4
Tanishq Raina <1ms23cs194@msrit.edu>
2025-10-31 17:58:11 +0530

    Add .gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
-
-print("The sum of two numbers is:",(a+b))
+c = int(input("Enter third number:"))
+print("The sum of three numbers is:",(a+b+c))
